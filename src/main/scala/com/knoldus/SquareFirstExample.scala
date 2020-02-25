d36a53b4ef48930f603cd546b8c47a34a0876648
nischalvasisth <nischalvasisth6@gmail.com>
2020-02-25 15:37:53 +0530

    second-coomit

--- src/main/scala/com/knoldus/SquareFirstExample.scala
+++ src/main/scala/com/knoldus/SquareFirstExample.scala
@@ -5,9 +5,3 @@ class SquareFirstExample(val side:Double) extends FourSideFigure {
   override val width :Double=side
 }
 
-object SquareFirstExample extends  App{
-  val abc:FourSideFigure=new SquareFirstExample(10)
-  val abb:FourSideFigure=new RectangleFirstExample(10,3)
-  println(abc.getArea)
-  println(abb.getArea)
-}
